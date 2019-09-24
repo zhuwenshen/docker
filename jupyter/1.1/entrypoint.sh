0c5ac1f00ac551d142d46092956b8afd20cad45b
zhuwenshen <1204621904@qq.com>
2019-09-24 18:09:35 +0800

    jupyter1.1版

---
+++ jupyter/1.1/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ $PASSWD != '' && ! -f "/init_pws.txt" ]; then	
+	touch "/init_pws.txt"
+	PASSWD=$(python -c 'from notebook.auth import passwd;import os; print(passwd(os.popen("echo -n $PASSWD").read()));')
+	echo "c.NotebookApp.password=u'${PASSWD}'" > ~/.jupyter/jupyter_notebook_config.py
+	echo "生成密码$PASSWD"		
+fi
+cat ~/.jupyter/jupyter_notebook_config.py
+jupyter notebook --allow-root --no-browser --ip='0.0.0.0' --port=6006 --notebook-dir=/app/data
